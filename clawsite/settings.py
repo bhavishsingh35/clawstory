a470ec6da1f2cc5774d9af2d050fdbafdead89c0
bhavishsingh35 <bhavishsingh35@gmail.com>
2026-01-09 18:55:07 +0530

    trace

--- clawsite/settings.py
+++ clawsite/settings.py
@@ -25,11 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+# DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG=True
+# ALLOWED_HOSTS = os.environ.get(
+#     "ALLOWED_HOSTS", ""
+# ).split(",")
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", ""
-).split(",")
+ALLOWED_HOSTS=["*"]
 #comment
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "CSRF_TRUSTED_ORIGINS", ""
