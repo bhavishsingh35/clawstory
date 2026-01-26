7283488421a0b15a423aca306b6aa2ed20115050
bhavishsingh35 <bhavishsingh35@gmail.com>
2026-01-27 01:52:09 +0530

    pro-lives

--- clawsite/settings.py
+++ clawsite/settings.py
@@ -210,3 +210,17 @@ if not DEBUG:
 # DEFAULT PRIMARY KEY
 # =================================================
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
