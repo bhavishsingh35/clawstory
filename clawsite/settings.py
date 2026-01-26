3dc8bfd8d10d9cb4dab2f0e60a4fc6d8c04ec028
bhavishsingh35 <bhavishsingh35@gmail.com>
2026-01-27 01:58:40 +0530

    production-level-fix

--- clawsite/settings.py
+++ clawsite/settings.py
@@ -106,14 +106,25 @@ TEMPLATES = [
 # =================================================
 # DATABASE
 # =================================================
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG,
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL:
+    # Render / Postgres
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    # Local / SQLite
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # =================================================
 # PASSWORD VALIDATION
